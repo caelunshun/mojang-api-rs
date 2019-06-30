9967f840c58e8930d385ccc2f35ed861c434f8a7
caelunshun <caelum12321@gmail.com>
2019-06-30 12:12:18 -0600

    Prepare for actual writing

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ extern crate serde_derive;
 
 use std::collections::HashMap;
 
-const STATUS_URL: &'static str = "https://status.mojang.com/check";
-const API_URL: &'static str = "https://api.mojang.com";
+#[cfg(test)]
+use mockito;
+use serde_json::Value;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Error {
     RequestFailed,
     BadResponse,
@@ -13,8 +15,8 @@ pub enum Error {
 
 /// The status values returned by the API status
 /// check.
-#[derive(Clone, Copy, Debug)]
-pub enum APIStatusValue {
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum APIStatus {
     /// The service has no issues and is running
     /// fine.
     NoIssues,
@@ -24,12 +26,12 @@ pub enum APIStatusValue {
     Unavailable,
 }
 
-impl APIStatusValue {
+impl APIStatus {
     fn from_string(s: &str) -> Result<Self, Error> {
         match s {
-            "red" => Ok(APIStatusValue::Unavailable),
-            "yellow" => Ok(APIStatusValue::SomeIssues),
-            "green" => Ok(APIStatusValue::NoIssues),
+            "red" => Ok(APIStatus::Unavailable),
+            "yellow" => Ok(APIStatus::SomeIssues),
+            "green" => Ok(APIStatus::NoIssues),
             _ => Err(Error::BadResponse),
         }
     }
@@ -39,29 +41,83 @@ impl APIStatusValue {
 /// to `APIStatusValues` to represent the response.
 #[derive(Clone, Debug)]
 pub struct APIStatusResponse {
-    statuses: HashMap<String, APIStatusValue>,
+    statuses: HashMap<String, APIStatus>,
 }
 
 impl APIStatusResponse {
-    pub fn get_status(&self, service: &str) -> Option<&APIStatusValue> {
-        self.statuses.get(service)
+    pub fn get_status(&self, service: &str) -> Option<APIStatus> {
+        self.statuses.get(service).cloned()
     }
 }
 
 pub fn api_status() -> Result<APIStatusResponse, Error> {
+    #[cfg(not(test))]
+    let url = "https://status.mojang.com/check";
+    #[cfg(test)]
+    let url = &format!("{}{}", &mockito::server_url(), "/check");
+
     let client = reqwest::Client::new();
-    let mut res = client.get(STATUS_URL)
+    let mut res = client
+        .get(url)
         .send()
         .map_err(|_| Error::RequestFailed)?;
 
-    let map: HashMap<String, String> = res.json().map_err(|_| Error::BadResponse)?;
-    let statuses = map.into_iter().map(|(k, v)| {
-        let val = APIStatusValue::from_string(&v)?;
-        Ok((k, val))
-    }).collect::<Result<HashMap<String, APIStatusValue>, Error>>();
+    let map: HashMap<String, String> = {
+        let mut m = HashMap::new();
+        let val: Value = res.json().map_err(|_| Error::BadResponse)?;
+        let arr = val.as_array().ok_or_else(|| Error::BadResponse)?;
+        for obj in arr {
+            let obj = obj.as_object().ok_or_else(|| Error::BadResponse)?;
+            let (key, val) = obj.iter().next().ok_or_else(|| Error::BadResponse)?;
+            let val = val.as_str().ok_or_else(|| Error::BadResponse)?;
+            m.insert(key.to_string(), val.to_string());
+        }
+        m
+    };
+
+    let statuses = map
+        .into_iter()
+        .map(|(k, v)| {
+            let val = APIStatus::from_string(&v)?;
+            Ok((k, val))
+        })
+        .collect::<Result<HashMap<String, APIStatus>, Error>>();
 
     match statuses {
         Ok(m) => Ok(APIStatusResponse { statuses: m }),
         Err(e) => Err(e),
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::mock;
+
+    #[test]
+    fn test_status_value_from_str() {
+        assert_eq!(
+            APIStatus::from_string("green"),
+            Ok(APIStatus::NoIssues)
+        );
+        assert_eq!(
+            APIStatus::from_string("yellow"),
+            Ok(APIStatus::SomeIssues)
+        );
+        assert_eq!(
+            APIStatus::from_string("red"),
+            Ok(APIStatus::Unavailable)
+        );
+    }
+
+    #[test]
+    fn test_api_status() {
+        let _m = mock("GET", "/check")
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_body("[{\"minecraft.net\": \"yellow\"}]");
+
+        assert_eq!(api_status().unwrap().get_status("minecraft.net"), Some(APIStatus::SomeIssues));
+    }
+
 }
