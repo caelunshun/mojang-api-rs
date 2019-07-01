a3f9aaad8421acf7ef579eba49cbc6f1a0480369
caelunshun <caelum12321@gmail.com>
2019-07-01 11:16:11 -0600

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate serde_derive;
 
-use openssl::sha::Sha1;
 use num_bigint::BigInt;
+use openssl::sha::Sha1;
 
 use reqwest::Client;
 
@@ -33,11 +33,16 @@ pub enum AuthError {
 }
 
 pub fn server_auth(username: &str, server_hash: &str) -> Result<ServerAuthResponse, AuthError> {
-    let url = format!("https://sessionserver.mojang.com/session/minecraft/hashJoined?username={}&serverId={}", username, server_hash);
+    let url = format!(
+        "https://sessionserver.mojang.com/session/minecraft/hashJoined?username={}&serverId={}",
+        username, server_hash
+    );
     let client = Client::new();
 
-    let mut res = client.get(&url)
-        .send().map_err(|_| AuthError::RequestFailed)?;
+    let mut res = client
+        .get(&url)
+        .send()
+        .map_err(|_| AuthError::RequestFailed)?;
     let res: ServerAuthResponse = res.json().map_err(|_| AuthError::RequestFailed)?;
 
     Ok(res)
@@ -50,7 +55,6 @@ pub fn server_hash(server_id: &str, shared_secret: [u8; 16], pub_key: &[u8]) -> 
     hasher.update(pub_key);
 
     mc_hexdigest(hasher)
-
 }
 
 fn mc_hexdigest(hasher: Sha1) -> String {
@@ -69,14 +73,23 @@ mod tests {
         // Examples from wiki.vg
         let mut hasher = Sha1::new();
         hasher.update(b"Notch");
-        assert_eq!(mc_hexdigest(hasher), "4ed1f46bbe04bc756bcb17c0c7ce3e4632f06a48");
+        assert_eq!(
+            mc_hexdigest(hasher),
+            "4ed1f46bbe04bc756bcb17c0c7ce3e4632f06a48"
+        );
 
         let mut hasher = Sha1::new();
         hasher.update(b"jeb_");
-        assert_eq!(mc_hexdigest(hasher), "-7c9d5b0044c130109a5d7b5fb5c317c02b4e28c1");
+        assert_eq!(
+            mc_hexdigest(hasher),
+            "-7c9d5b0044c130109a5d7b5fb5c317c02b4e28c1"
+        );
 
         let mut hasher = Sha1::new();
         hasher.update(b"simon");
-        assert_eq!(mc_hexdigest(hasher), "88e16a1019277b15d58faf0541e11910eb756f6");
+        assert_eq!(
+            mc_hexdigest(hasher),
+            "88e16a1019277b15d58faf0541e11910eb756f6"
+        );
     }
 }
