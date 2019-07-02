c47536336b5ea23770e19ebf9e7d970799dc156c
caelunshun <caelum12321@gmail.com>
2019-07-01 18:58:28 -0600

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! This crate implements a simple API for
+//! interfacing with the Mojang API.
+//!
+//! Currently, only server-side authentication
+//! has been implemented, but more functions will
+//! be added in the near future.
+
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
@@ -8,32 +15,61 @@ use openssl::sha::Sha1;
 
 use reqwest::Client;
 
-/*#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Error {
-    RequestFailed,
-    BadResponse,
-}*/
-
+/// The session server's response to
+/// an authentication request.
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct ServerAuthResponse {
+    /// The player's UUID, encoded
+    /// as a string without dashes.
+    /// The string can be serialized
+    /// using `Uuid::from_str()`.
     pub id: String,
+    /// The player's username.
     pub name: String,
+    /// A list of properties sent by the session
+    /// server. Typically, a "textures"
+    /// property is sent containing the player's
+    /// skin.
     pub properties: Vec<ServerAuthProperty>,
 }
 
+/// A property, as returned by the session server
+/// during authentication. Typically, a "textures"
+/// property is received in the authentication response
+/// which contains base64-encoded JSON data. See wiki.vg
+/// for more information.
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct ServerAuthProperty {
+    /// The name of the property.
     pub name: String,
+    /// The value of the property.
+    /// In the case of the "textures" property,
+    /// this is sent as a base64-encoded
+    /// JSON dictionary containing the skin's
+    /// URL/
     pub value: String,
+    /// The property's signature as signed
+    /// by Yggdrasil's private key.
+    /// This is used by the client to ensure
+    /// validity of the data.
     pub signature: String,
 }
 
+/// An error during authentication.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AuthError {
+    /// Something went wrong with the request.
+    /// For example, the Internet might not be
+    /// reacheable.
     RequestFailed,
+    /// Authentication failed. If this error
+    /// is returned, servers should kick clients.
     AuthFailed,
 }
 
+/// Performs a server-side authentication request as documented
+/// at wiki.vg/Authentication_scheme. `server_hash` can be obtained
+/// by calling `server_hash()`.
 pub fn server_auth(username: &str, server_hash: &str) -> Result<ServerAuthResponse, AuthError> {
     let url = format!(
         "https://sessionserver.mojang.com/session/minecraft/hasJoined?username={}&serverId={}&unsigned=false",
@@ -54,6 +90,10 @@ pub fn server_auth(username: &str, server_hash: &str) -> Result<ServerAuthRespon
     Ok(res)
 }
 
+/// Computes the "server hash" required for authentication
+/// based on the server ID, the shared secret, and the server's
+/// public RSA key. On modern Minecraft versions, the server ID
+/// is always an empty string.
 pub fn server_hash(server_id: &str, shared_secret: [u8; 16], pub_key: &[u8]) -> String {
     let mut hasher = Sha1::new();
     hasher.update(server_id.as_bytes());
