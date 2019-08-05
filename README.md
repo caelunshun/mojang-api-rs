96227104987d2829a73bf84c47788be8c29c982c
caelunshun <caelum12321@gmail.com>
2019-08-05 11:46:19 -0600

    Complete 0.2.0

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ### mojang-api
-This crate implements a simple API for interfacing with the Mojang API. Currently, only
-server-side authentication is implemented, but additional functionality will
-be added in the future.
+![Documentation](https://docs.rs/mojang-api/badge.svg)
+[![crates.io](https://meritbadge.herokuapp.com/mojang-api)](https://crates.io/crates/mojang-api)
+
+This crate offers a simple interface for utilizing the Mojang API. It utilizes
+experimental async/await syntax, allowing for clean, asynchronous requests.
+
+Currently, the following mechanisms are supported:
+* Obtaining the "server hash" required for authenticating with Mojang.
+* Running server-side authentication of a client.
+
+In the future, additional functionality will be added.
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,57 @@
 //! A simple, easy-to-use library for interfacing with the Mojang API.
+//!
+//! All functions involving IO in this crate are asynchronous and utilize
+//! async/await. As a result, you will have to use nightly Rust until async/await
+//! is stabilized.
+//!
+//! This crate provides a number of functions:
+//!
+//! * Server-side authentication with the Mojang API, used to verify
+//! that clients have logged in correctly. This is available using the
+//! [`server_auth`](fn.server_auth.html) function.
+//! * Obtaining the "server hash" required for authentication, available using
+//! [`server_hash`](fn.server_hash.html). Since Mojang uses abnormal hash digests
+//! for obtaining the value, this crate provides a simple way to obtain it.
+//!
+//! # Examples
+//! Authenticating a client on a server:
+//! ```no_run
+//! #![feature(async_await)]
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), mojang_api::Error> {
+//! # let shared_secret = [0; 16];
+//! # let username = "test";
+//! # let public_key = &[0];
+//!
+//! // Obtain the "server hash"
+//! let server_hash = mojang_api::server_hash(
+//!     "", // Note that the "server ID" is always an empty string
+//!     shared_secret,
+//!     public_key,
+//! );
+//!
+//! // Make the API request
+//! let response = mojang_api::server_auth(&server_hash, username).await?;
+//!
+//! // Now do something with it...
+//! # Ok(())
+//! # }
+//! ```
 
 #![feature(async_await)]
 #![forbid(unsafe_code, missing_docs, missing_debug_implementations, warnings)]
 #![doc(html_root_url = "https://docs.rs/mojang-api/0.2.0")]
 
-use futures::compat::Future01CompatExt;
+use bytes::Buf;
+use hyper::{Body, Client};
+use hyper_tls::HttpsConnector;
 use num_bigint::BigInt;
-use reqwest::r#async::Client;
 use serde::{Deserialize, Serialize};
 use sha1::Sha1;
 use std::fmt;
 use std::fmt::{Display, Formatter};
 use std::io;
+use std::string::FromUtf8Error;
 use uuid::Uuid;
 
 type StdResult<T, E> = std::result::Result<T, E>;
@@ -25,15 +65,23 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     /// Indicates that an IO error occurred.
     Io(io::Error),
-    /// Indicates that an error using `reqwest` occurred.
-    Reqwest(reqwest::Error),
+    /// Indicates that an HTTP error occurred.
+    Http(hyper::Error),
+    /// Indicates that the UTF8 bytes failed to parse.
+    Utf8(FromUtf8Error),
+    /// Indicates that the response included malformed JSON.
+    /// This could also indicate that, for example, authentication
+    /// failed, because the response would have unexpected fields.
+    Json(serde_json::Error),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> StdResult<(), fmt::Error> {
         match self {
             Error::Io(e) => write!(f, "{}", e)?,
-            Error::Reqwest(e) => write!(f, "{}", e)?,
+            Error::Http(e) => write!(f, "{}", e)?,
+            Error::Utf8(e) => write!(f, "{}", e)?,
+            Error::Json(e) => write!(f, "{}", e)?,
         }
         Ok(())
     }
@@ -92,42 +140,19 @@ pub struct ProfileProperty {
 /// information.
 ///
 /// # Examples
-/// ```
+/// ```no_run
 /// #![feature(async_await)]
 /// # #[tokio::main]
 /// # async fn main() -> Result<(), mojang_api::Error> {
-/// # use mojang_api::{ServerAuthResponse, ProfileProperty};
-/// # use uuid::Uuid;
-/// # let uuid = Uuid::new_v4();
-/// # let username = "test_";
-/// # let prop_name = "test_prop";
-/// # let prop_val = "test_val";
-/// # let prop_signature = "jioiodqwqiowoiqf";
-/// # fn server_hash() -> String {
-/// #    mojang_api::server_hash("", [0; 16], &[1])
-/// # }
-/// # let prop = ProfileProperty { name: prop_name.to_string(), value: prop_val.to_string(), signature: prop_signature.to_string() };
-/// # let response = ServerAuthResponse {
-/// #    name: username.to_string(),
-/// #    id: uuid,
-/// #    properties: vec![prop],
-/// # };
-/// # let _m = mockito::mock("GET", "/").with_body(serde_json::to_string(&response).unwrap()).create();
-/// # let server_hash = server_hash();
+/// # fn server_hash() -> String { "".to_string() }
+/// # fn username() -> String { "".to_string() }
+/// // Obtain the server hash and username...
+/// let hash = server_hash();
+/// let username = username();
 ///
-/// let result = mojang_api::server_auth(&server_hash, username).await?;
-///
-/// assert_eq!(result.id, uuid);
-/// assert_eq!(result.name, username);
-/// assert_eq!(result.properties.len(), 1);
-///
-/// let prop = result.properties.first().unwrap();
-///
-/// assert_eq!(prop.name, prop_name);
-/// assert_eq!(prop.value, prop_val);
-/// assert_eq!(prop.signature, prop_signature);
-///
-/// Ok(())
+/// // Make the API request
+/// let response = mojang_api::server_auth(&hash, &username).await?;
+/// # Ok(())
 /// # }
 /// ```
 pub async fn server_auth(server_hash: &str, username: &str) -> Result<ServerAuthResponse> {
@@ -144,15 +169,18 @@ pub async fn server_auth(server_hash: &str, username: &str) -> Result<ServerAuth
         server_hash
     );
 
-    let client = Client::new();
-    let mut res = client
-        .get(&url)
-        .send()
-        .compat()
-        .await
-        .map_err(Error::Reqwest)?;
+    let connector = HttpsConnector::new(1).unwrap();
 
-    let json = res.json().compat().await.map_err(Error::Reqwest)?;
+    let client = Client::builder().build::<_, Body>(connector);
+    let response = client
+        .get(url.parse().unwrap())
+        .await
+        .map_err(Error::Http)?;
+
+    let mut body = response.into_body();
+
+    let response = body_to_string(&mut body).await?;
+    let json: ServerAuthResponse = serde_json::from_str(&response).map_err(Error::Json)?;
 
     Ok(json)
 }
@@ -189,6 +217,22 @@ fn mc_hexdigest(hasher: Sha1) -> String {
     format!("{:x}", bigint)
 }
 
+/// Reads a `Body` into a string.
+async fn body_to_string(body: &mut Body) -> Result<String> {
+    let mut s = String::new();
+
+    while let Some(next) = body.next().await {
+        let mut chunk = next.map_err(Error::Http)?;
+        let mut vec = vec![0; chunk.len()];
+        chunk.copy_to_slice(&mut vec);
+
+        let string = String::from_utf8(vec).map_err(Error::Utf8)?;
+        s.push_str(&string);
+    }
+
+    Ok(s)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -216,5 +260,47 @@ mod tests {
             mc_hexdigest(hasher),
             "88e16a1019277b15d58faf0541e11910eb756f6"
         );
+    }
+
+    #[tokio::test]
+    async fn test_server_auth() -> Result<()> {
+        use uuid::Uuid;
+
+        let uuid = Uuid::new_v4();
+        let username = "test_";
+        let prop_name = "test_prop";
+        let prop_val = "test_val";
+        let prop_signature = "jioiodqwqiowoiqf";
+
+        let prop = ProfileProperty {
+            name: prop_name.to_string(),
+            value: prop_val.to_string(),
+            signature: prop_signature.to_string(),
+        };
+
+        let response = ServerAuthResponse {
+            name: username.to_string(),
+            id: uuid,
+            properties: vec![prop],
+        };
+
+        let _m = mockito::mock("GET", "/")
+            .with_body(serde_json::to_string(&response).unwrap())
+            .create();
+        let hash = server_hash("", [0; 16], &[0]);
+
+        let result = server_auth(&hash, username).await?;
+
+        assert_eq!(result.id, uuid);
+        assert_eq!(result.name, username);
+        assert_eq!(result.properties.len(), 1);
+
+        let prop = result.properties.first().unwrap();
+
+        assert_eq!(prop.name, prop_name);
+        assert_eq!(prop.value, prop_val);
+        assert_eq!(prop.signature, prop_signature);
+
+        Ok(())
     }
 }
